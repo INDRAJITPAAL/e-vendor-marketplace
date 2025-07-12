86a80f9a3376a2bb851d0152cdd721d0fa2f2ec9
INDRAJITPAAL <indrajitpal389@gmail.com>
2025-07-12 20:54:30 +0545

    button effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import { Textarea } from "@/components/ui/textarea";
+import { Checkbox } from "@/components/ui/checkbox";
 
 
 export default function Home() {
   return (
-    <div>
-   
+    <div className="flex flex-col m-10 gap-y-2 " >
+      <Button variant={"elevated"} >i am a button</Button>
+      <Textarea>i am a button</Textarea>
+      <Checkbox/>
     </div>
   );
 }
